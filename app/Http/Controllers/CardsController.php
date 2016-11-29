0a912f57ab002d4b0f2b5e93096e96bde6262d18
JWizerd <jwodke@outlook.com>
2016-11-29 11:52:25 -0700

    elegants elegant relationship with typehinting and routes model binding

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Card;
 use DB;
 use Illuminate\Http\Request;
 
 class CardsController extends Controller
 {
   public function index() {
-    $cards = DB::table('cards')->get();
+    $cards = Card::all();
     return view('cards.index', compact('cards'));
+  }
+
+  public function show(Card $card) {
+    return view('cards.show', compact('card'));
   }
 }
