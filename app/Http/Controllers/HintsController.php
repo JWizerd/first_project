9890c113737263f63924a9a73433cca00032b684
JWizerd <jwodke@outlook.com>
2016-11-30 09:11:37 -0700

    refactoring our post method in hints controller

--- app/Http/Controllers/HintsController.php
+++ app/Http/Controllers/HintsController.php
@@ -10,8 +10,15 @@ use Illuminate\Http\Request;
 class HintsController extends Controller
 {
   public function store(Request $request, Card $card) {
-    $hint = new Hint;
-    $hint->body = $request->body;
+    // Long Hand Method for Creating new Hints
+    // $hint = new Hint;
+    // $hint->body = $request->body;
+    // $card->hints()->save($hint);
+    // return back();
+
+
+    // Short Hand Method for Creating new Hints
+    $hint = new Hint(['body' => $request->body]);
     $card->hints()->save($hint);
     return back();
   }
