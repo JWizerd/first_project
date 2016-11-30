8b4d6244daa2834f1d6aa19a8844dde6e233ee31
JWizerd <jwodke@outlook.com>
2016-11-30 09:20:17 -0700

    i love laravel. so many neat tricks to abstract the bullshit away and to make your code extra pretty.

--- app/Http/Controllers/HintsController.php
+++ app/Http/Controllers/HintsController.php
@@ -11,20 +11,24 @@ class HintsController extends Controller
 {
   public function store(Request $request, Card $card) {
     // Long Hand Method for Creating new Hints
-    // $hint = new Hint;
-    // $hint->body = $request->body;
-    // $card->hints()->save($hint);
-    // return back();
-
+      // $hint = new Hint;
+      // $hint->body = $request->body;
+      // $card->hints()->save($hint);
+      // return back();
 
     // Short Hand Method for Creating new Hints
-    // $hint = new Hint(['body' => $request->body]);
-    // $card->hints()->save($hint);
-    // return back();
+      // $hint = new Hint(['body' => $request->body]);
+      // $card->hints()->save($hint);
+      // return back();
 
-    // OR use eloquents built relational methods for creating hints relating to a specific card. the hints() method instantiates a new hint use HasMany eloquent method. 
+    // OR use eloquents built relational methods for creating hints relating to a specific card. the hints() method instantiates a new hint use HasMany eloquent method.
 
-    $card->hints()->create(['body' => $request->body]);
+      // $card->hints()->create(['body' => $request->body]);
+      // return back();
+
+    // Even Further, use the all() method on the request to get all data from the post request. Make sure that you have $fillable set for data input to eliminate any malicious attempts.
+
+    $card->hints()->create($request->all());
     return back();
   }
 }
